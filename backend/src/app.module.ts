bd5c2969fe4a66113a9a4dbe05addd28f4f56f17
桂川直也 <katsuragawanaoya@katsuragawanaoyanoMacBook-Pro.local>
2022-07-24 23:13:47 +0900

    create gql-server

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './components/posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: path.join(
+        process.cwd(),
+        'src/generated/graphql/schema.gql',
+      ),
+      sortSchema: true,
+    }),
+    PostsModule, // これ
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
