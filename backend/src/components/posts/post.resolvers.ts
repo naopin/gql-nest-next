a20e9df2d039e99cf1abee58166ba150e254a8fd
桂川直也 <katsuragawanaoya@katsuragawanaoyanoMacBook-Pro.local>
2022-08-04 23:34:40 +0900

    フロント、バックエンド繋ぎこみ

--- backend/src/components/posts/post.resolvers.ts
+++ backend/src/components/posts/post.resolvers.ts
@@ -1,12 +1,14 @@
+import { PrismaService } from './../prisma/prisma.service';
 import { Args, Query, Resolver } from '@nestjs/graphql';
 import { PostModel } from './interfaces/post.model';
+import { GetPostsArgs } from './interfaces/get-posts-connection.args';
 
 @Resolver((of) => PostModel)
 export class PostsResolver {
-  constructor() {}
+  constructor(private readonly prisma: PrismaService) {}
 
-  @Query(() => [PostModel], { name: 'posts', nullable: true })
-  async getPosts() {
+  @Query(() => [PostModel], { name: 'fixedPosts', nullable: true })
+  async getPostsByFixedData() {
     return [
       {
         id: '1',
@@ -17,5 +19,27 @@ export class PostsResolver {
         title: 'GraphQL is so good.',
       },
     ];
+  }
+
+  @Query(() => [PostModel], { name: 'prismaPosts', nullable: true })
+  async getPostsByPrisma() {
+    return this.prisma.post.findMany();
+  }
+
+  @Query(() => [PostModel], { name: 'posts', nullable: true })
+  async getPosts(@Args() args: GetPostsArgs) {
+    return this.prisma.post.findMany({
+      where: {
+        type: args.type
+          ? {
+              in: args.type,
+            }
+          : undefined,
+        published: true, // ついでに指定。公開ブログへ渡すデータなのでtrue固定にしちゃう
+      },
+      orderBy: {
+        publishDate: 'desc',
+      },
+    });
   }
 }
